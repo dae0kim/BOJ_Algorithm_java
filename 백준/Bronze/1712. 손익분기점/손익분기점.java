108a744dc9828f8b844d51d266b792b473b5013c
dae0kim <94046871+dae0kim@users.noreply.github.com>
2022-11-08 13:43:16 +0900

    [Bronze II] Title: 손익분기점, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine()," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		int answer;
+		
+		if ( b >= c){
+			answer = -1;
+		}else{
+			answer = (a/(c-b))+1;
+		}
+		System.out.println(answer);
+    }
+}
