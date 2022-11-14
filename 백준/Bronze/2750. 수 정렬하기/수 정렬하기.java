946036835fb373cabb5cf0a4d76b6f5677281524
dae0kim <94046871+dae0kim@users.noreply.github.com>
2022-11-15 08:45:49 +0900

    [Bronze II] Title: 수 정렬하기, Time: 172 ms, Memory: 16248 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+        
+
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N];
+
+		for (int i=0; i < N; i++){
+			st = new StringTokenizer(br.readLine());
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		for (int i=0; i<arr.length; i++){
+			bw.write(arr[i]+"\n");
+		}
+		br.close();
+
+		bw.flush();
+		bw.close();
+    }
+}
